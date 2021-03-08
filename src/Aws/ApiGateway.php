f1953ba3732213a2cb1c6d21d9a34a12c8ea8668
Pierre <dev@nettools.ovh>
2021-03-08 13:30:03 +0100

    bulkSend

--- src/Aws/ApiGateway.php
+++ src/Aws/ApiGateway.php
@@ -35,6 +35,17 @@ class ApiGateway implements \Nettools\SMS\SMSGateway {
 	
 	
 	
+	public function sanitizeSender($sender)
+	{
+		// if there are unwanted characters in sender (AWS does not permit characters other than letters and digits ; spaces or dots are forbidden)
+		if ( $this->sanitizeSenderId && preg_match('/[^A-Za-z0-9]/', $sender) )
+			return str_replace(' ', '', ucwords(strtolower(preg_replace('/[^A-Za-z0-9]/', ' ', $sender))));
+		else
+			return $sender;
+	}
+	
+	
+	
 	/**
 	 * Send SMS to several recipients
 	 *
@@ -47,48 +58,21 @@ class ApiGateway implements \Nettools\SMS\SMSGateway {
 	 */
 	function send($msg, $sender, array $to, $transactional = true)
 	{
-		// if there are unwanted characters in sender (AWS does not permit characters other than letters and digits ; spaces or dots are forbidden)
-		if ( $this->sanitizeSenderId && preg_match('/[^A-Za-z0-9]/', $sender) )
-			$sender = str_replace(' ', '', ucwords(strtolower(preg_replace('/[^A-Za-z0-9]/', ' ', $sender))));
+		if ( count($to) == 0 )
+			return 0;
 		
 		
-		try
+		// publish message to several recipients ; aws doesn't allow sending to many recipients with publish to phonenumber
+		if ( count($to) > 1 )
+			return $this->bulkSend($msg, $sender, $to, $transactional);
+		else
 		{
-			// publish message to several recipients
-			if ( count($to) > 1 )
+			// if there are unwanted characters in sender (AWS does not permit characters other than letters and digits ; spaces or dots are forbidden)
+			$sender = $this->sanitizeSender($sender);
+
+			
+			try
 			{
-			// creating topic
-			$topic = $this->client->createTopic([
-					'Name' => self::AWS_TOPIC_PREFIX . uniqid()
-				]);
-
-
-			// subscribing all recipients in $to array to topic created
-			foreach ( $to as $num )
-			$this->client->subscribe([
-				   'Protocol' => 'sms',
-				   'Endpoint' => $num,
-				   'TopicArn' => $topic['TopicArn']
-				]);
-
-
-			// publish message to topic
-			$ret = $this->client->publish([
-					'Message'			=> $msg,
-					'TopicArn'		=> $topic['TopicArn'],
-					'MessageAttributes'	=> [
-						'AWS.SNS.SMS.SenderID'	=> [
-							'DataType'		=> 'String',
-							'StringValue'	=> $sender
-						],
-						'AWS.SNS.SMS.SMSType'	=> [
-							'DataType'		=> 'String',
-							'StringValue'	=> ($transactional ? 'Transactional':'Promotional')
-						]
-					]
-				]);
-			}
-			else
 				// publish message to a single recipient
 				$ret = $this->client->publish([
 						'Message'			=> $msg,
@@ -104,18 +88,102 @@ class ApiGateway implements \Nettools\SMS\SMSGateway {
 							]
 						]
 					]);
+			}
+			catch(\Aws\Exception\AwsException $e)
+			{
+				throw new \Nettools\SMS\SMSException($e->getMessage());
+			}
+
+
+			if ( $ret['MessageId'] )
+				return count($to);
+
+
+			// we shouldn't arrive here
+			throw new \Nettools\SMS\SMSException('SNS unkown error : ' . print_r($ret));
+		}
+	}
+	
+	
+	
+	/**
+	 * Send SMS to a lot of recipients (this is more optimized that calling `send` with a big array of recipients)
+	 *
+	 * @param string $msg 
+	 * @param string $sender
+	 * @param string[] $to Big array of recipients, numbers in international format +xxyyyyyyyyyyyyy (ex. +33612345678)
+	 * @param bool $transactional True if message sent is transactional ; otherwise i's promotional)
+	 * @return int Returns the number of SMS sent (a multi-sms message count as as many message)
+	 */
+	function bulkSend($msg, $sender, array $to, $transactional = true)
+	{
+		// if there are unwanted characters in sender (AWS does not permit characters other than letters and digits ; spaces or dots are forbidden)
+		$sender = $this->sanitizeSender($sender);
+
+		
+		
+		try
+		{
+			// creating topic
+			$topic = $this->client->createTopic([
+					'Name' => self::AWS_TOPIC_PREFIX . uniqid()
+				]);
+
+
+			// subscribing all recipients in $to array to topic created
+			foreach ( $to as $num )
+				$this->client->subscribe([
+					   'Protocol' => 'sms',
+					   'Endpoint' => $num,
+					   'TopicArn' => $topic['TopicArn']
+					]);
+
+
+			// publish message to topic
+			$ret = $this->client->publish([
+					'Message'			=> $msg,
+					'TopicArn'			=> $topic['TopicArn'],
+					'MessageAttributes'	=> [
+						'AWS.SNS.SMS.SenderID'	=> [
+							'DataType'		=> 'String',
+							'StringValue'	=> $sender
+						],
+						'AWS.SNS.SMS.SMSType'	=> [
+							'DataType'		=> 'String',
+							'StringValue'	=> ($transactional ? 'Transactional':'Promotional')
+						]
+					]
+				]);
 		}
 		catch(\Aws\Exception\AwsException $e)
 		{
 			throw new \Nettools\SMS\SMSException($e->getMessage());
 		}
 		
+
 		
 		if ( $ret['MessageId'] )
 			return count($to);
-		
-		
+
+
+		// we shouldn't arrive here
 		throw new \Nettools\SMS\SMSException('SNS unkown error : ' . print_r($ret));
+	}
+	
+	
+	
+	/**
+	 * Send SMS to a lot of recipients by downloading a CSV file
+	 *
+	 * @param string $msg 
+	 * @param string $sender
+	 * @param string $url Url of CSV file with recipients, numbers in international format +xxyyyyyyyyyyyyy (ex. +33612345678), first row is column headers (1 column title 'Number')
+	 * @param bool $transactional True if message sent is transactional ; otherwise i's promotional)
+	 * @return int Returns the number of SMS sent (a multi-sms message count as as many message)
+	 */
+	function bulkSendFromHttp($msg, $sender, $url, $transactional = true)
+	{
+		throw new SMSException('bulkSendFromHttp not implemented');
 	}
 }
 
